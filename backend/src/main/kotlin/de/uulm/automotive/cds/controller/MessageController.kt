01f655992215a089dba8b906a353d44cab2ba3f3
Fabian <fabian.egl@uni-ulm.de>
2020-05-26 17:39:40 +0200

    Added view for message

--- backend/src/main/kotlin/de/uulm/automotive/cds/controller/MessageController.kt
+++ backend/src/main/kotlin/de/uulm/automotive/cds/controller/MessageController.kt
@@ -2,10 +2,18 @@ package de.uulm.automotive.cds.controller
 
 import de.uulm.automotive.cds.models.Message
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.*
 
 @Controller
 class MessageController {
+
+    @GetMapping("/message")
+    fun messageForm(model: Model): String {
+        model["title"] = "Message"
+        return "message"
+    }
 
     @PostMapping("/message")
     fun relayMessage(@ModelAttribute message: Message) {
