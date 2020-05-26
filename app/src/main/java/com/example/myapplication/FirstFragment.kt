960a0b182e5acda24d18160448879029bb92691b
Tim <rock-4-ever2@gmx.de>
2020-05-26 20:53:02 +0200

    Changed message subscription mechanics to amqp.

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -1,23 +1,26 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import org.eclipse.paho.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.IMqttActionListener
-import org.eclipse.paho.client.mqttv3.IMqttToken
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttException
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.rabbitmq.client.CancelCallback
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
+import com.rabbitmq.client.Delivery
+import java.nio.charset.Charset
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private val QUEUE_NAME: String = "hello"
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -31,9 +34,36 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            amqpSub()
         }
     }
 
+    fun amqpSub() {
+        //probably required to be able to start a new thread to be able to wait asynchronously for the callback
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
+        //setup connection params
+        val factory = ConnectionFactory()
+        factory.host = "134.60.157.15"
+        factory.username = "android_cl"
+        factory.password = "supersecure"
+
+        //open new connection to broker
+        val connection = factory.newConnection()
+        val channel = connection.createChannel()
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null)
+
+        //define callback which should be executed when message is received from queue
+        val deliverCallback =
+            DeliverCallback { _: String?, delivery: Delivery ->
+                val message = String(delivery.body, Charset.forName("UTF-8"))
+                activity?.runOnUiThread { Toast.makeText(context, message, Toast.LENGTH_SHORT).show() }
+            }
+
+        Log.d("AMQP"," [*] Waiting for messages. To exit press CTRL+C")
+        //start subscription on queue
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, CancelCallback {  })
+    }
 }
