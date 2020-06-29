f09152727799b013c210f0c35657207c106206cc
Tim <rock-4-ever2@gmx.de>
2020-06-29 22:08:33 +0200

    refactor(Backend): Moved the scheduling times into application.properties
    Added two new application.properties files corresponding to the profiles
    dev and prod. Each file contains individual times settings for the
    message scheduling job
    
    Closes #25

--- backend/src/main/kotlin/de/uulm/automotive/cds/services/SchedulingService.kt
+++ backend/src/main/kotlin/de/uulm/automotive/cds/services/SchedulingService.kt
@@ -3,13 +3,10 @@ package de.uulm.automotive.cds.services
 import de.uulm.automotive.cds.entities.Message
 import de.uulm.automotive.cds.repositories.MessageRepository
 import de.uulm.automotive.cds.repositories.SignUpRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
-
-const val sendMessageIntervalInSeconds : Long = 60
-const val removeTokenIntervalInSeconds : Long = 3600
-const val secondToMillis: Long = 1000
 
 /**s
  * A Service class that takes care of all actions that execute on a scheduled time.
@@ -19,14 +16,16 @@ const val secondToMillis: Long = 1000
  * @property messageService
  */
 @Component
-class SchedulingService(private val messageRepository: MessageRepository, private val tokenRepository: SignUpRepository, private val messageService: MessageService) {
+class SchedulingService(private val messageRepository: MessageRepository, private val tokenRepository: SignUpRepository, private val messageService: MessageService,
+                        @Value("\${pruning.interval.seconds: 60}000")
+                        val removeTokenIntervalInSeconds: Long) {
 
     /**
      * Sends all Messages that are not sent and were scheduled to send before the current time.
      *
      * @return Boolean True if all Messages were sent
      */
-    @Scheduled(fixedRate = sendMessageIntervalInSeconds * secondToMillis)
+    @Scheduled(fixedRateString = "\${sending.interval.seconds: 60}000")
     fun sendMessages(): Boolean {
         val messages = messageRepository.findAllByIsSentFalseOrderByStarttimeAsc()
         messages.forEach { message: Message ->
@@ -44,7 +43,7 @@ class SchedulingService(private val messageRepository: MessageRepository, privat
      * Scheduled in an hourly interval.
      * Removes all SignUp tokens that have not been used in the last hour.
      */
-    @Scheduled(fixedRate = removeTokenIntervalInSeconds * secondToMillis)
+    @Scheduled(fixedRateString = "\${pruning.interval.seconds: 3600}000")
     fun removeOldSignUpTokens() {
         val tokens = tokenRepository.findAll()
         val time = LocalDateTime.now().minusSeconds(removeTokenIntervalInSeconds)
