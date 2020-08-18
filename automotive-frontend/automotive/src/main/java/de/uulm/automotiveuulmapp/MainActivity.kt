5d764f20c9bf5cfcbf1045ac2bd42619a1101b7c
Tim <rock-4-ever2@gmx.de>
2020-08-18 22:10:43 +0200

    Merged dev into navigation_bar

--- automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/MainActivity.kt
+++ automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package de.uulm.automotiveuulmapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import de.uulm.automotiveuulmapp.topicFragment.TopicFragment
 
 class MainActivity : AppCompatActivity(){
+    companion object {
+        const val REQUEST_LOCATION_PERMISSIONS_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +38,19 @@ class MainActivity : AppCompatActivity(){
 
         navBar.setOnNavigationItemReselectedListener {
             //do nothing when reselected
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSIONS_CODE
+            )
         }
     }
 
