2911d31a350289c93121df33eadf44bfcb153f92
Fabian <fabian.egl@uni-ulm.de>
2020-09-08 21:30:35 +0200

    feat: Added Routing and the Topic and Property Components

---
+++ webview/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {MessageComponent} from "./message/message.component";
+import {PropertyComponent} from "./property/property.component";
+import {TopicComponent} from "./topic/topic.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/message', pathMatch: 'full'},
+  { path: 'message', component: MessageComponent },
+  { path: 'property', component: PropertyComponent },
+  { path: 'topic', component: TopicComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
