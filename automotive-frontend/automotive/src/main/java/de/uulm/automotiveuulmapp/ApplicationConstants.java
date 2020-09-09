9cab11c84055187ee770d33df6bd517a22673396
jfsLaptop <JulianStier@web.de>
2020-09-09 18:53:01 +0200

    feat(Android Frontend Local and global configurations): Using local or global backendurl depending on the chosen Build Variant
    closes: #82

--- automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/ApplicationConstants.java
+++ automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/ApplicationConstants.java
@@ -17,11 +17,10 @@ public abstract class ApplicationConstants {
     public static final String DEVICE_TYPE = "Android Emulator";
 
     // REST Endpoints
-    // environment variable BACKENDURL is set in the run configuration.
-    // If you need to edit, do it in the run configuration
+    // BuildConfig variable BACKEND_URL is set in build.gradle and depends on the used Build Variant
     // values at the moment: http://10.0.2.2:8080 (local backend),
     // http://elbitbackend.schuster.domains:8085 (global backend)
-    private static final String APPLICATION_URL = System.getenv("BACKENDURL");
+    private static String APPLICATION_URL = BuildConfig.BACKEND_URL; // System.getenv("BACKENDURL");
     public static final String ENDPOINT_SIGNUP = APPLICATION_URL + "/signup/";
     public static final String ENDPOINT_TOPIC = APPLICATION_URL + "/topic";
 }
