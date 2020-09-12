9e733559bf478633a36c33195e1d4795df063ff3
Fabian Egl <fabian.egl@uni-ulm.de>
2020-09-12 18:15:28 +0200

    Update ApplicationConstants.java

--- automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/ApplicationConstants.java
+++ automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/ApplicationConstants.java
@@ -19,7 +19,7 @@ public abstract class ApplicationConstants {
     // REST Endpoints
     // BuildConfig variable BACKEND_URL is set in build.gradle and depends on the used Build Variant
     // values at the moment: http://10.0.2.2:8080 (local backend),
-    // http://elbitbackend.schuster.domains:8085 (global backend)
+    // https://elbitbackend.schuster.domains (global backend)
     private static String APPLICATION_URL = BuildConfig.BACKEND_URL;
     public static final String ENDPOINT_SIGNUP = APPLICATION_URL + "/signup/";
     public static final String ENDPOINT_TOPIC = APPLICATION_URL + "/topic";
