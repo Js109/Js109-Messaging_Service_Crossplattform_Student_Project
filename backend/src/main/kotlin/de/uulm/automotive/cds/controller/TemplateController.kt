d7b0e6c3a68cba660a3779807e6f48d59b59964d
Michael Scheurer <michael.scheurer@uni-ulm.de>
2020-09-14 11:33:58 +0200

    feat(backend): added templates to backend
    added TemplateMessage class which extends Message and is stored in
    seperat table in jpa unit;
    added a repository and controller for
    template
    
    Closes #91

---
+++ backend/src/main/kotlin/de/uulm/automotive/cds/controller/TemplateController.kt
@@ -0,0 +1,25 @@
+package de.uulm.automotive.cds.controller
+
+import de.uulm.automotive.cds.entities.TemplateMessage
+import de.uulm.automotive.cds.repositories.TemplateRepository
+import org.springframework.web.bind.annotation.*
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/template")
+class TemplateController(private val repository: TemplateRepository){
+    @GetMapping
+    fun getTemplates() : Iterable<TemplateMessage> {
+        return repository.findAllByOrderByTemplateNameAsc()
+    }
+
+    @PostMapping
+    fun postTemplate(@RequestBody template: TemplateMessage) {
+        repository.save(template)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteTemplate(@PathVariable id: Long) {
+        repository.deleteById(id)
+    }
+}
