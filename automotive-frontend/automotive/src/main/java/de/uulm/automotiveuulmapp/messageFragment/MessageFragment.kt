87ce9604e0b20411342455c923524ab8dd2ff35f
Tim <rock-4-ever2@gmx.de>
2020-09-23 17:50:27 +0200

    feat(android): Messages now can be deleted by swiping
    Added swipe gesture to the message recycler view items as well as a
    confirmation dialogue to prevent accidental deletion.
    
    BREAKING CHANGE: Messages now can be deleted by swiping

--- automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/messageFragment/MessageFragment.kt
+++ automotive-frontend/automotive/src/main/java/de/uulm/automotiveuulmapp/messageFragment/MessageFragment.kt
@@ -1,19 +1,23 @@
 package de.uulm.automotiveuulmapp.messageFragment
 
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
-import androidx.core.view.isVisible
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.uulm.automotiveuulmapp.BaseFragment
 import de.uulm.automotiveuulmapp.R
-import de.uulm.automotiveuulmapp.messages.messagedb.MessageDao
 import de.uulm.automotiveuulmapp.messages.messagedb.MessageDatabase
+import kotlinx.coroutines.launch
 
 class MessageFragment : BaseFragment() {
+
+    private lateinit var messageAdapter: MessageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +30,7 @@ class MessageFragment : BaseFragment() {
 
         val searchView = view.findViewById<SearchView>(R.id.messageSearch)
 
-        val messageAdapter = MessageAdapter(searchView, MessageDatabase.getDaoInstance(recyclerView.context), activity)
+        messageAdapter = MessageAdapter(searchView, MessageDatabase.getDaoInstance(recyclerView.context), activity)
 
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context).apply { reverseLayout = true}
@@ -44,7 +48,58 @@ class MessageFragment : BaseFragment() {
             }
         })
 
+        // binding "swipe handler" to recyclerview
+        itemTouchHelperCallback.attachToRecyclerView(recyclerView)
         return view
     }
 
+    // used to enable and handle swipe gestures
+    private val itemTouchHelperCallback = ItemTouchHelper(
+        object : SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // not implemented
+                return false
+            }
+
+            // is triggered when the element has reached its swipe destination
+            // calls a dialogue, if the element should be deleted
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val dialog = MessageDeletionDialogFragment (
+                    onConfirm = {
+                        messageAdapter.removeMessage(viewHolder.adapterPosition)
+                    },
+                    onDeny = fun (){
+                        // just resets the slided element to its original position
+                        messageAdapter.notifyQueryChanged()
+                    })
+                dialog.show(parentFragmentManager, "MessageViewDeletionDialogue")
+            }
+
+            // Simply avoids the swiped element to swipe out of bounds
+            // adjust dX to change the distance the element swipes to
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX/10,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+            }
+        }
+    )
 }
