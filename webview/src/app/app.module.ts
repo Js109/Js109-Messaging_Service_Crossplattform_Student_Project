c7558b42892405a51c77b29b1f743263cce3ec62
jfsLaptop <JulianStier@web.de>
2020-10-06 14:38:56 +0200

    feat(WebApp): make program working again after merge conflicts

--- webview/src/app/app.module.ts
+++ webview/src/app/app.module.ts
@@ -30,7 +30,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {NgxMatDatetimePickerModule} from '@angular-material-components/datetime-picker';
 import {NgxMatMomentModule} from '@angular-material-components/moment-adapter';
-import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
