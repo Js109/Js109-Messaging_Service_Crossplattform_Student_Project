7892ce79331816d2d46c9f8b501e41a71ab80c28
Tim <rock-4-ever2@gmx.de>
2020-10-10 14:52:33 +0200

    refactor(android, backend): Moved amq access data to configuration
    Moved amq access data to run-config (backend) and buildconfig (android)
    to be able to change it dynamically without having to go through the
    actual implementation. Changed broker IP to domain name.
    
    Closes #160

--- backend/src/main/kotlin/de/uulm/automotive/cds/services/AmqpChannelService.kt
+++ backend/src/main/kotlin/de/uulm/automotive/cds/services/AmqpChannelService.kt
@@ -4,6 +4,7 @@ package de.uulm.automotive.cds.services
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 /**
@@ -11,15 +12,20 @@ import org.springframework.stereotype.Component
  * Other components can use this one to communicate with the broker.
  */
 @Component
-class AmqpChannelService() {
+class AmqpChannelService(
+        @Value("\${amq.broker.url}") private val address: String,
+        @Value("\${amq.broker.username}") private val username: String,
+        @Value("\${amq.broker.password}") private val password: String
+) {
 
     private val connection: Connection
 
     init {
+
         val factory = ConnectionFactory()
-        factory.host = "134.60.157.15"
-        factory.username = "android_cl"
-        factory.password = "supersecure"
+        factory.host = address
+        factory.username = username
+        factory.password = password
         connection = factory.newConnection()
     }
 
